47368c753c8671ce834868a9e6dc6b691950a4fd
Akhil <akhilmahendra035@gmail.com>
2024-09-14 14:43:57 +0530

    Update vault provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "vault" {
-  address         = "https://vault-internal.akhilsaidevops.online"
+  address         = "https://vault-internal.akhilsaidevops.online:8200"
   skip_tls_verify = true
   token           = var.vault_token
 }
