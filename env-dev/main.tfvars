de61f2a230a5aa5faf8fc13c3f91c1c8c0347a91
Akhil <akhilmahendra035@gmail.com>
2024-09-28 12:32:02 +0530

    Create VPC  nd subnet

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,3 +1,7 @@
-env           = "dev"
-instance_type = "t3.small"
-zone_id       = "Z08716401XK2LTO5VD1E1"
+env            = "dev"
+instance_type  = "t3.small"
+zone_id        = "Z08716401XK2LTO5VD1E1"
+
+# vpc
+vpc_cidr_block    = "10.10.0.0/24"
+subnet_cidr_block = "10.10.0.0/24"
--- main.tf
+++ main.tf
@@ -1,30 +1,37 @@
-module "frontend" {
-  depends_on = [module.backend]
+# module "frontend" {
+#   depends_on = [module.backend]
+#
+#   source        = "./modules/app"
+#   instance_type = var.instance_type
+#   component     = "frontend"
+#   env           = var.env
+#   zone_id       = var.zone_id
+#   vault_token   = var.vault_token
+# }
+#
+# module "backend" {
+#   depends_on = [module.mysql]
+#
+#   source        = "./modules/app"
+#   instance_type = var.instance_type
+#   component     = "backend"
+#   env           = var.env
+#   zone_id       = var.zone_id
+#   vault_token   = var.vault_token
+# }
+#
+# module "mysql" {
+#   source        = "./modules/app"
+#   instance_type = var.instance_type
+#   component     = "mysql"
+#   env           = var.env
+#   zone_id       = var.zone_id
+#   vault_token   = var.vault_token
+# }
 
-  source        = "./modules/app"
-  instance_type = var.instance_type
-  component     = "frontend"
-  env           = var.env
-  zone_id       = var.zone_id
-  vault_token   = var.vault_token
-}
-
-module "backend" {
-  depends_on = [module.mysql]
-
-  source        = "./modules/app"
-  instance_type = var.instance_type
-  component     = "backend"
-  env           = var.env
-  zone_id       = var.zone_id
-  vault_token   = var.vault_token
-}
-
-module "mysql" {
-  source        = "./modules/app"
-  instance_type = var.instance_type
-  component     = "mysql"
-  env           = var.env
-  zone_id       = var.zone_id
-  vault_token   = var.vault_token
+module "vpc" {
+  source            = "./modules/vpc"
+  env               = var.env
+  vpc_cidr_block    = var.vpc_cidr_block
+  subnet_cidr_block = var.subnet_cidr_block
 }
