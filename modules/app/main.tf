4c4a805dec51a2987471919f5b57027c5f0ed6c7
Akhil <akhilmahendra035@gmail.com>
2024-09-28 15:36:32 +0530

    Enable App modules to create

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -9,15 +9,15 @@ resource "aws_security_group" "main" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-}
 
-egress {
-  from_port        = 0
-  to_port          = 0
-  protocol         = "-1"
-  cidr_blocks      = ["0.0.0.0/0"]
-}
-}
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
 
   tags = {
     Name = "${var.component}-${var.env}-sg"
