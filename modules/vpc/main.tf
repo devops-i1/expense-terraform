b11ea6bfcf17d2761bb85a271a74b1b1fcf96684
Akhil <akhilmahendra035@gmail.com>
2024-10-01 19:08:08 +0530

    Adding public subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -66,9 +66,9 @@ resource "aws_subnet" "db" {
 }
 
 resource "aws_subnet" "public" {
-  count             = length(var.db_subnets)
+  count             = length(var.public_subnets)
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.db_subnets[count.index]
+  cidr_block        = var.public_subnets[count.index]
   availability_zone = var.availability_zones[count.index]
 
   tags = {
