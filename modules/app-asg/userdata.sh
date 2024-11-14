b635b5f90ead070e65db38373923b1ccb260958c
Akhil <akhilmahendra035@gmail.com>
2024-11-14 17:50:01 +0530

    Skipping dependencies install during deployment

--- modules/app-asg/userdata.sh
+++ modules/app-asg/userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ansible-pull -i localhost, -U https://github.com/devops-i1/expense-ansible.git get-secrets.yml -e env=${env} -e role_name=${component} -e vault_token=${vault_token} &>>/opt/ansible.log
-ansible-pull -i localhost, -U https://github.com/devops-i1/expense-ansible.git expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/devops-i1/expense-ansible.git expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json -e only_deployment=false &>>/opt/ansible.log
 
