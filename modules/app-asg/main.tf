7fade9c0828fa6e8cf28c5e5f26ef40284ca7df2
Akhil <akhilmahendra035@gmail.com>
2024-11-22 10:46:07 +0530

    Add env to prometheus

--- modules/app-asg/main.tf
+++ modules/app-asg/main.tf
@@ -80,6 +80,12 @@ resource "aws_autoscaling_group" "main" {
     value               = "yes"
     propagate_at_launch = true
   }
+
+  tag {
+    key                 = "env"
+    value               = var.env
+    propagate_at_launch = true
+  }
 }
 
 resource "aws_autoscaling_policy" "main" {
