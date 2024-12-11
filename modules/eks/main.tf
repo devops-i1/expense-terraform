0929c3b6826e21ead53431e155c74826eaab7deb
Akhil <akhilmahendra035@gmail.com>
2024-12-11 14:09:44 +0530

    Add KMS to node group of EKS

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -14,6 +14,21 @@ resource "aws_eks_cluster" "cluster" {
   }
 }
 
+resource "aws_launch_template" "main" {
+  name                   = "eks-${var.env}"
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size           = 100
+      encrypted             = true
+      kms_key_id            = var.kms_key_id
+      delete_on_termination = true
+    }
+  }
+}
+
 resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = "${var.env}-eks-ng"
@@ -21,6 +36,11 @@ resource "aws_eks_node_group" "main" {
   subnet_ids      = var.subnet_ids
   capacity_type   = "SPOT"
   instance_types  = ["t3.large"]
+
+  launch_template {
+    name    = "eks-${var.env}"
+    version = "$Latest"
+  }
 
   scaling_config {
     desired_size = 1
